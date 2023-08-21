39baa24777e6ef8e52617a14f43c746ff7b9daa6
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-21 19:48:29 +0530

    home and login routers added

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        loadComponent: () => import ('./pages/home/home.component') .then ( (c) => c.HomeComponent),
+    },
+    {
+        path: 'login',
+        loadComponent: () => import ('./pages/login/login.component') .then ( (c) => c.LoginComponent),
+    }
+];
