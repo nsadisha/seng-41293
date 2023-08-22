0816ede5a9d839825e3348f43a1a9b1311dcdd17
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-22 18:09:52 +0530

    AuthModule imported

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getEnvPath } from './app.helper';
+import { AuthModule } from './auth/auth.module';
 
 const envFilePath: string = getEnvPath(`${__dirname}/environments`);
 
 @Module({
   imports: [
     UserModule,
+    AuthModule,
     ConfigModule.forRoot({ envFilePath }),
     // MongooseModule.forRoot('')
     MongooseModule.forRootAsync({
