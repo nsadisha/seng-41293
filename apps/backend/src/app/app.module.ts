b0b6edac4a2bae87698e3af37e9fca678f370fea
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-22 14:22:06 +0530

    initial code removed

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb+srv://backend_user:backend_password@seng-db.lfnofmf.mongodb.net/')],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
