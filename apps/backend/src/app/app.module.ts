a10ad6c75f2bb32a89ae98b63d9c5c5e9903d488
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-22 16:50:45 +0530

    get database url from env

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getEnvPath } from './app.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/environments`);
+
 @Module({
   imports: [
     UserModule,
-    MongooseModule.forRoot('mongodb+srv://backend_user:backend_password@seng-db.lfnofmf.mongodb.net/seng')
+    ConfigModule.forRoot({ envFilePath }),
+    // MongooseModule.forRoot('')
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URL')
+      })
+    })
   ],
 })
 export class AppModule {}
