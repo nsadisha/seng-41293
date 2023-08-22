72585db97d9c0a4fdb4e956a98df584afe8710e3
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-23 00:14:44 +0530

    login function

--- apps/backend/src/app/auth/auth.module.ts
+++ apps/backend/src/app/auth/auth.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-// import { UserService } from '../user/user.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfigModule } from '../config/config.module';
+import { AppConfigService } from '../config/config.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule,
+    AppConfigModule,
+    JwtModule.registerAsync({
+      imports: [AppConfigModule],
+      inject: [AppConfigService],
+      useFactory: (configService: AppConfigService) => ({
+        global: true,
+        secret: configService.jwtSecret,
+        signOptions: { expiresIn: configService.jwtExpiration },
+      })
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/backend/src/app/auth/auth.service.ts
+++ apps/backend/src/app/auth/auth.service.ts
@@ -1,20 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UserService } from '../user/user.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { User } from '../user/schema/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import { UserDto } from '../user/dto/user.dto';
+import { SuccessAuthDto } from './dto/success-auth.dto';
 
 @Injectable()
 export class AuthService {
   
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private jwtService: JwtService
   ){}
   
   register(createAuthDto: CreateAuthDto) {
     return this.userService.create(createAuthDto);
   }
 
-  login(loginAuthDto: LoginAuthDto){
+  async login(loginAuthDto: LoginAuthDto){
+    const user = await this.userService.findUserWithEmail(loginAuthDto.email) as unknown as UserDto;
+    if(!user){
+      throw new NotFoundException('Email not found!');
+    }
+    
+    const isLoginSuccess: boolean = user.password === loginAuthDto.password;
 
+    if(!isLoginSuccess){
+      throw new UnauthorizedException('Invalid password!');
+    }
+
+    const payload = { sub: user._id, username: user.email };
+    const access_token = await this.jwtService.signAsync(payload);
+    const successResponse: SuccessAuthDto = {
+      success: isLoginSuccess,
+      email: user.email,
+      access_token: access_token
+    };
+
+    return successResponse;
   }
 }
