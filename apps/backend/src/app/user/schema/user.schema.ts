a9639a524859495fff756967b735c02e8ed5d860
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-22 16:06:27 +0530

    id removed

--- apps/backend/src/app/user/schema/user.schema.ts
+++ apps/backend/src/app/user/schema/user.schema.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { IUser } from '@seng-41293/model';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class User implements IUser{
+    @Prop()
+    name: string;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    password: string;
+}
+
+export type UserDocument = HydratedDocument<User>;
+export const UserSchema = SchemaFactory.createForClass(User);
