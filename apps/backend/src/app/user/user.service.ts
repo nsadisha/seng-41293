d8510a93d255dfd7c3de53d28778bc8e68364f0e
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-23 00:14:20 +0530

    unique email check

--- apps/backend/src/app/user/user.service.ts
+++ apps/backend/src/app/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -10,8 +10,17 @@ export class UserService {
 
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
   
-  create(createUserDto: CreateUserDto): Promise<User> {
-    return this.userModel.create(createUserDto);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const newUser = new this.userModel(createUserDto);
+      return await newUser.save();
+    } catch (error) {
+      if (error.code === 11000 && error.keyPattern.email) {
+        throw new ConflictException('Email address is already in use.');
+      } else {
+        throw error;
+      }
+    }
   }
 
   findAll() {
