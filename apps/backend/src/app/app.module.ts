3c9d33a873193b38666e0386c7803889791e2e16
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-22 14:43:17 +0530

    user resource added

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://backend_user:backend_password@seng-db.lfnofmf.mongodb.net/')],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot('mongodb+srv://backend_user:backend_password@seng-db.lfnofmf.mongodb.net/')
+  ],
 })
 export class AppModule {}
