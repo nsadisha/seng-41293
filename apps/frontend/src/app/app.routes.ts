ab840ca29c18d0a25b0ad2fb6db7d55382382ffd
Sadisha Nimsara <nsadisha@gmail.com>
2023-08-24 14:05:14 +0530

    register route adde

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -8,5 +8,9 @@ export const appRoutes: Route[] = [
     {
         path: 'login',
         loadComponent: () => import ('./pages/login/login.component') .then ( (c) => c.LoginComponent),
+    },
+    {
+        path: 'register',
+        loadComponent: () => import ('./pages/register/register.component') .then ( (c) => c.RegisterComponent),
     }
 ];
